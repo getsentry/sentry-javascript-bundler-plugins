798c41e78dd003614d437fb37f32703bd212b47e
Luca Forstner <luca.forstner@sentry.io>
2022-06-26 14:27:57 +0000

    build: Add eslint setup

---
+++ packages/unplugin/.eslintrc.js
@@ -0,0 +1,20 @@
+const jestPackageJson = require("jest/package.json");
+
+/** @type {import('eslint').ESLint.Options} */
+module.exports = {
+  root: true,
+  extends: ["local/jest", "local/base"],
+  ignorePatterns: [".eslintrc.js", "dist", "jest.config.js", "rollup.config.js"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./test/tsconfig.json"],
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    jest: {
+      version: jestPackageJson.version,
+    },
+  },
+};
