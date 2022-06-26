3d88a8c06bcd400b4d9a38165d39d15ecdf09857
Luca Forstner <luca.forstner@sentry.io>
2022-06-26 10:53:35 +0000

    build(unplugin): Add build and build:watch commands

---
+++ packages/unplugin/rollup.config.js
@@ -0,0 +1,36 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import packageJson from "./package.json";
+
+const input = ["src/index.ts"];
+
+const extensions = [".js", ".ts"];
+
+export default {
+  input,
+  external: [...Object.keys(packageJson.dependencies)],
+  plugins: [
+    resolve({ extensions }),
+    commonjs(),
+    babel({
+      extensions,
+      babelHelpers: "bundled",
+      include: ["src/**/*"],
+    }),
+  ],
+  output: [
+    {
+      file: packageJson.module,
+      format: "esm",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+};
