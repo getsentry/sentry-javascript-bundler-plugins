b2afba10e7d0959c8f04b298f72e181469f83592
Luca Forstner <luca.forstner@sentry.io>
2022-08-22 12:37:19 +0200

    Document hooks

--- packages/unplugin/src/index.ts
+++ packages/unplugin/src/index.ts
@@ -38,12 +38,26 @@ const unplugin = createUnplugin<Options>((options) => {
   return {
     name: "sentry-plugin",
     enforce: "pre", // needed for Vite to call resolveId hook
-    resolveId(id, _importer, { isEntry }) {
+
+    /**
+     * In the sentry-unplugin, this hook is responsible for creating a Set containing the entrypoints as absolute paths.
+     *
+     * @param id For imports: The absolute path of the module to be imported. For entrypoints: The path the user defined as entrypoint - may also be relative.
+     * @param importer For imports: The absolute path of the module that imported this module. For entrypoints: `undefined`.
+     * @param options Additional information to use for making a resolving decision.
+     * @returns undefined.
+     */
+    resolveId(id, importer, { isEntry }) {
       debugLog(
-        `Called "resolveId": ${JSON.stringify({ id, importer: _importer, options: { isEntry } })}`
+        `Called "resolveId": ${JSON.stringify({ id, importer: importer, options: { isEntry } })}`
       );
 
+      // We only store the absolute path when we encounter an entrypoint
       if (isEntry) {
+        // If we're looking at an entrypoint, which is the case here, `id` is either an absolute path or a relative path.
+        // If it's an absolute path we can just store it. If it's a relative path, we can assume the path got defined
+        // from a config file and when bundlers are run via a config file the process CWD is usually the one the config
+        // file is located in, so we can simply join CWD and id and we get the absolute path.
         const entrypoint = path.normalize(path.isAbsolute(id) ? id : path.join(process.cwd(), id));
         entrypoints.add(entrypoint);
         debugLog(`Added entrypoint: ${entrypoint}`);
@@ -51,14 +65,30 @@ const unplugin = createUnplugin<Options>((options) => {
 
       return undefined;
     },
+
+    /**
+     * Determines whether we want to transform a module.
+     *
+     * @param id Always the absolute (fully resolved) path to the module.
+     * @returns `true` or `false` depending on whether we want to transform the module. For the sentry-unplugin we only
+     * want to transform entrypoints.
+     */
     transformInclude(id) {
-      const shouldTransform = entrypoints.has(id);
+      const shouldTransform = entrypoints.has(path.normalize(id));
 
       debugLog(`Called "transformInclude": ${JSON.stringify({ id })}`);
       debugLog(`Will transform "${id}": ${String(shouldTransform)}`);
 
       return shouldTransform;
     },
+
+    /**
+     * Responsible for injecting the global release value code.
+     *
+     * @param code Unprocessed code of the module.
+     * @param id Always the absolute (fully resolved) path to the module.
+     * @returns Code and source map if we decide to inject code. `undefined` otherwise.
+     */
     transform(code, id) {
       if (entrypoints.has(path.normalize(id))) {
         const ms = new MagicString(code);
@@ -76,10 +106,10 @@ const unplugin = createUnplugin<Options>((options) => {
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const sentryVitePlugin: (options?: Options | undefined) => any = unplugin.vite;
+export const sentryVitePlugin: (options: Options) => any = unplugin.vite;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const sentryRollupPlugin: (options?: Options | undefined) => any = unplugin.rollup;
+export const sentryRollupPlugin: (options: Options) => any = unplugin.rollup;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const sentryWebpackPlugin: (options?: Options | undefined) => any = unplugin.webpack;
+export const sentryWebpackPlugin: (options: Options) => any = unplugin.webpack;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const sentryEsbuildPlugin: (options?: Options | undefined) => any = unplugin.esbuild;
+export const sentryEsbuildPlugin: (options: Options) => any = unplugin.esbuild;
