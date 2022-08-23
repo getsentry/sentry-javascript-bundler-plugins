7197b9515adf21b9930ec0cd7a3d742988aeb93e
Lukas Stracke <lukas.stracke@sentry.io>
2022-08-23 13:39:38 +0200

    extract auth token

--- packages/playground/vite.config.smallNodeApp.js
+++ packages/playground/vite.config.smallNodeApp.js
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   plugins: [
     sentryVitePlugin({
-      authToken: "420cd2442b634005afd39e5074034873b476018960a5469e96a1300e3d398ea4",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
       org: "lms-testorg-9m",
       project: "hackweek-node-sample-app",
       debug: true,
