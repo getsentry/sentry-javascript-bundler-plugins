b0b0e1578d487dbad51e64d7ae56bb344df81569
Lukas Stracke <lukas.stracke@sentry.io>
2022-08-23 14:05:37 +0200

    fix type error in basic release injection test

--- packages/integration-tests/fixtures/basic-release-injection/setup.ts
+++ packages/integration-tests/fixtures/basic-release-injection/setup.ts
@@ -4,4 +4,4 @@ import { createCjsBundles } from "../../utils/create-cjs-bundles";
 const entryPointPath = path.resolve(__dirname, "./input/entrypoint.js");
 const outputDir = path.resolve(__dirname, "./out");
 
-createCjsBundles(entryPointPath, outputDir, { release: "I AM A RELEASE!" });
+createCjsBundles(entryPointPath, outputDir, { release: "I AM A RELEASE!", include: "" });
