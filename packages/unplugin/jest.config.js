4d96e30dead147d48b662eb37f9e59c64c683ae1
Luca Forstner <luca.forstner@sentry.io>
2022-08-27 10:36:06 +0200

    fix: Inject global package version in tests

--- packages/unplugin/jest.config.js
+++ packages/unplugin/jest.config.js
@@ -3,9 +3,21 @@ const packageJson = require("./package.json");
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
-  },
-  globals: {
-    __PACKAGE_VERSION__: packageJson.version,
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          transform: {
+            optimizer: {
+              globals: {
+                vars: {
+                  __PACKAGE_VERSION__: packageJson.version,
+                },
+              },
+            },
+          },
+        },
+      },
+    ],
   },
 };
