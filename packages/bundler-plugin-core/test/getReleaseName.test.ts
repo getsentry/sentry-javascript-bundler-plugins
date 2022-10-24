dc0e817db885ef5eda9bea978bb4a922dd248bcb
Luca Forstner <luca.forstner@sentry.io>
2022-10-24 13:46:52 +0200

    Fix tests typing

--- packages/bundler-plugin-core/test/getReleaseName.test.ts
+++ packages/bundler-plugin-core/test/getReleaseName.test.ts
@@ -1,4 +1,4 @@
-import { getReleaseName } from "../src/getReleaseName";
+import { detectRelease } from "../src/detect-release";
 import * as fs from "fs";
 import * as child_process from "child_process";
 jest.mock("fs");
@@ -28,54 +28,50 @@ describe("environmental getReleaseName", () => {
     const sha = "c3f235fc86f1c4007e3a218ec82d666586e73cbf";
     (mockedChildProcess.execSync as jest.Mock).mockReturnValue(sha);
 
-    expect(getReleaseName()).toBe(sha);
+    expect(detectRelease()).toBe(sha);
   });
 
   it("throws an error if no release information could be found", () => {
     (mockedFs.existsSync as jest.Mock).mockReturnValueOnce(false);
 
-    expect(getReleaseName).toThrow("Could not return a release name");
-  });
-
-  it("adheres to user defined release name", () => {
-    const releaseName = "USER_DEFINED_this-is-my-custom-release";
-
-    expect(getReleaseName(releaseName)).toBe(releaseName);
+    expect(detectRelease).toThrow(
+      'Could not automatically determine release name. Please provide a release identifier via the "release" option.'
+    );
   });
 
   it("adheres to process.env.SENTRY_RELEASE", () => {
     const releaseName = "SENTRY_RELEASE_string";
     process.env["SENTRY_RELEASE"] = releaseName;
 
-    expect(getReleaseName()).toBe(releaseName);
+    expect(detectRelease()).toBe(releaseName);
   });
 
   it("adheres to Heroku: process.env.SOURCE_VERSION", () => {
     const releaseName = "SOURCE_VERSION_string";
     process.env["SOURCE_VERSION"] = releaseName;
 
-    expect(getReleaseName()).toBe(releaseName);
+    expect(detectRelease()).toBe(releaseName);
   });
 
   it("adheres to Heroku: process.env.HEROKU_SLUG_COMMIT", () => {
     const releaseName = "HEROKU_SLUG_COMMIT_string";
     process.env["HEROKU_SLUG_COMMIT"] = releaseName;
 
-    expect(getReleaseName()).toBe(releaseName);
+    expect(detectRelease()).toBe(releaseName);
   });
 
   it("adheres to AWS: process.env.CODEBUILD_RESOLVED_SOURCE_VERSION", () => {
     const releaseName = "CODEBUILD_RESOLVED_SOURCE_VERSION_string";
     process.env["CODEBUILD_RESOLVED_SOURCE_VERSION"] = releaseName;
 
-    expect(getReleaseName()).toBe(releaseName);
+    expect(detectRelease()).toBe(releaseName);
   });
 
   it("adheres to Vercel: process.env.VERCEL_GIT_COMMIT_SHA", () => {
     const releaseName = "VERCEL_GIT_COMMIT_SHA_string";
     process.env["VERCEL_GIT_COMMIT_SHA"] = releaseName;
 
-    expect(getReleaseName()).toBe(releaseName);
+    expect(detectRelease()).toBe(releaseName);
   });
 
   it("allows SENTRY_RELEASE to take precedence over other env vars", () => {
@@ -84,15 +80,6 @@ describe("environmental getReleaseName", () => {
     process.env["VERCEL_GIT_COMMIT_SHA"] = vercelReleaseName;
     process.env["SENTRY_RELEASE"] = sentryReleaseName;
 
-    expect(getReleaseName()).toBe(sentryReleaseName);
-  });
-
-  it("allows custom release name to take precedence over other env vars", () => {
-    const vercelReleaseName = "VERCEL_GIT_COMMIT_SHA_string";
-    const sentryReleaseName = "SENTRY_RELEASE_string";
-    process.env["VERCEL_GIT_COMMIT_SHA"] = vercelReleaseName;
-    process.env["SENTRY_RELEASE"] = sentryReleaseName;
-
-    expect(getReleaseName("cutom_release_name")).toBe("cutom_release_name");
+    expect(detectRelease()).toBe(sentryReleaseName);
   });
 });
