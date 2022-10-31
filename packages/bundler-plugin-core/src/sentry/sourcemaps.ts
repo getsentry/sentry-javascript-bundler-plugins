c06b21fb4eb30d7d00f68c353094d0e24da90314
Lukas Stracke <lukas.stracke@sentry.io>
2022-10-31 14:39:47 +0100

    feat(core): Add `ignoreFile` option (sort of)

--- packages/bundler-plugin-core/src/sentry/sourcemaps.ts
+++ packages/bundler-plugin-core/src/sentry/sourcemaps.ts
@@ -3,6 +3,8 @@ import fs, { Stats } from "fs";
 import glob from "glob";
 import { InternalIncludeEntry } from "../options-mapping";
 
+import parseIgnoreFile from "parse-gitignore";
+
 export type FileRecord = {
   name: string;
   content: string;
@@ -28,9 +30,7 @@ export function getFiles(includePath: string, includeEntry: InternalIncludeEntry
   if (fileStat.isFile()) {
     files = [{ absolutePath, relativePath: path.basename(absolutePath) }];
   } else if (fileStat.isDirectory()) {
-    const absoluteIgnores = includeEntry.ignore.map((ignoreEntry) =>
-      path.join(process.cwd(), ignoreEntry)
-    );
+    const absoluteIgnores = getIgnoreEntries(includeEntry);
 
     files = glob
       .sync(path.join(absolutePath, "**"), {
@@ -50,8 +50,6 @@ export function getFiles(includePath: string, includeEntry: InternalIncludeEntry
     return includeEntry.ext.includes(path.extname(absolutePath));
   });
 
-  // TODO ignore files
-  // TODO ignorefile
   // TODO do sourcemap rewriting?
   // TODO do sourcefile rewriting? (adding source map reference to bottom - search for "guess_sourcemap_reference")
 
@@ -69,4 +67,44 @@ export function getFiles(includePath: string, includeEntry: InternalIncludeEntry
 
 function convertWindowsPathToPosix(windowsPath: string): string {
   return windowsPath.split(path.sep).join(path.posix.sep);
+}
+
+function getIgnoreEntries(includeEntry: InternalIncludeEntry): InternalIncludeEntry["ignore"] {
+  const absoluteIgnores = includeEntry.ignore.map((ignoreEntry) => {
+    if (ignoreEntry.startsWith("!")) {
+      return `!${path.join(process.cwd(), ignoreEntry.replace(/^!/, ""))}`;
+    }
+    return path.join(process.cwd(), ignoreEntry);
+  });
+
+  const absoluteIgnoreFileIgnores = getIgnoreEntriesFromIgnoreFile(includeEntry.ignoreFile);
+
+  return [...absoluteIgnores, ...absoluteIgnoreFileIgnores];
+}
+
+function getIgnoreEntriesFromIgnoreFile(ignoreFile: InternalIncludeEntry["ignoreFile"]): string[] {
+  // This is what parse-gitignore actually returns after version 2.0.0
+  // (including a few other properties we don't need).
+  // Using this as a type until the library updates its type declarations.
+  type ParseIgnoreReturn = {
+    patterns: string[];
+  };
+
+  if (!ignoreFile) {
+    return [];
+  }
+
+  // The parse-gitignore library's types declaration file was not updated to v2.0.0.
+  // Hence, we have to override its return value (and ignore the linter who rightfully complains)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const fileContent = parseIgnoreFile(fs.readFileSync(ignoreFile)) as unknown as ParseIgnoreReturn;
+  const patterns = fileContent.patterns;
+
+  return patterns.map((ignoreEntry) => {
+    const ignoreFileDir = path.dirname(path.resolve(ignoreFile));
+    if (ignoreEntry.startsWith("!")) {
+      return `!${path.join(ignoreFileDir, ignoreEntry.replace(/^!/, ""))}`;
+    }
+    return path.join(path.join(ignoreFileDir, ignoreEntry));
+  });
 }
