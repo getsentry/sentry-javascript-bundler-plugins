4818fee5b59c42b0100af607de407a2fd67a4dd6
Lukas Stracke <lukas.stracke@sentry.io>
2022-11-10 13:59:37 +0100

    ref(core): Refactor options validation

--- packages/bundler-plugin-core/src/sentry/releasePipeline.ts
+++ packages/bundler-plugin-core/src/sentry/releasePipeline.ts
@@ -13,40 +13,14 @@ import { addSpanToTransaction } from "./telemetry";
 export async function createNewRelease(options: InternalOptions, ctx: BuildContext): Promise<void> {
   const span = addSpanToTransaction(ctx, "function.plugin.create_release");
 
-  // TODO: pull these checks out of here and simplify them
-  if (options.authToken === undefined) {
-    ctx.logger.warn('Missing "authToken" option. Will not create release.');
-    return;
-  } else if (options.org === undefined) {
-    ctx.logger.warn('Missing "org" option. Will not create release.');
-    return;
-  } else if (options.project === undefined) {
-    ctx.logger.warn('Missing "project" option. Will not create release.');
-    return;
-  }
-
   await ctx.cli.releases.new(options.release);
 
   ctx.logger.info("Successfully created release.");
-
   span?.finish();
 }
 
 export async function uploadSourceMaps(options: InternalOptions, ctx: BuildContext): Promise<void> {
   const span = addSpanToTransaction(ctx, "function.plugin.upload_sourcemaps");
-
-  // TODO: pull these checks out of here and simplify them
-  if (options.authToken === undefined) {
-    ctx.logger.warn('Missing "authToken" option. Will not create release.');
-    return Promise.resolve();
-  } else if (options.org === undefined) {
-    ctx.logger.warn('Missing "org" option. Will not create release.');
-    return Promise.resolve();
-  } else if (options.project === undefined) {
-    ctx.logger.warn('Missing "project" option. Will not create release.');
-    return Promise.resolve();
-  }
-
   ctx.logger.info("Uploading Sourcemaps.");
 
   // Since our internal include entries contain all top-level sourcemaps options,
@@ -54,7 +28,6 @@ export async function uploadSourceMaps(options: InternalOptions, ctx: BuildConte
   await ctx.cli.releases.uploadSourceMaps(options.release, { include: options.include });
 
   ctx.logger.info("Successfully uploaded Sourcemaps.");
-
   span?.finish();
 }
 
@@ -73,20 +46,7 @@ export async function cleanArtifacts(options: InternalOptions, ctx: BuildContext
   const span = addSpanToTransaction(ctx, "function.plugin.clean_artifacts");
 
   if (options.cleanArtifacts) {
-    // TODO: pull these checks out of here and simplify them
-    if (options.authToken === undefined) {
-      ctx.logger.warn('Missing "authToken" option. Will not clean existing artifacts.');
-      return;
-    } else if (options.org === undefined) {
-      ctx.logger.warn('Missing "org" option. Will not clean existing artifacts.');
-      return;
-    } else if (options.project === undefined) {
-      ctx.logger.warn('Missing "project" option. Will not clean existing artifacts.');
-      return;
-    }
-
     await ctx.cli.releases.execute(["releases", "files", options.release, "delete", "--all"], true);
-
     ctx.logger.info("Successfully cleaned previous artifacts.");
   }
 
@@ -99,22 +59,16 @@ export async function setCommits(options: InternalOptions, ctx: BuildContext): P
   if (options.setCommits) {
     const { auto, repo, commit, previousCommit, ignoreMissing, ignoreEmpty } = options.setCommits;
 
-    if (auto || (repo && commit)) {
-      await ctx.cli.releases.setCommits(options.release, {
-        commit,
-        previousCommit,
-        repo,
-        auto,
-        ignoreMissing,
-        ignoreEmpty,
-      });
-      ctx.logger.info("Successfully set commits.");
-    } else {
-      ctx.logger.error(
-        "Couldn't set commits - neither the `auto` nor the `repo` and `commit` options were specified!",
-        "Make sure to either set `auto` to `true` or to manually set `repo` and `commit`."
-      );
-    }
+    await ctx.cli.releases.setCommits(options.release, {
+      commit,
+      previousCommit,
+      repo,
+      auto,
+      ignoreMissing,
+      ignoreEmpty,
+    });
+
+    ctx.logger.info("Successfully set commits.");
   }
 
   span?.finish();
@@ -126,22 +80,16 @@ export async function addDeploy(options: InternalOptions, ctx: BuildContext): Pr
   if (options.deploy) {
     const { env, started, finished, time, name, url } = options.deploy;
 
-    if (env) {
-      await ctx.cli.releases.newDeploy(options.release, {
-        env,
-        started,
-        finished,
-        time,
-        name,
-        url,
-      });
-      ctx.logger.info("Successfully added deploy.");
-    } else {
-      ctx.logger.error(
-        "Couldn't add deploy - the `env` option was not specified!",
-        "Make sure to set `deploy.env` (e.g. to 'production')."
-      );
-    }
+    await ctx.cli.releases.newDeploy(options.release, {
+      env,
+      started,
+      finished,
+      time,
+      name,
+      url,
+    });
+
+    ctx.logger.info("Successfully added deploy.");
   }
 
   span?.finish();
