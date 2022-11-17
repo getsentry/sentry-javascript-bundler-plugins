a73d0ddca82db45b3279b13d220d14fdbbe6c409
Luca Forstner <luca.forstner@sentry.io>
2022-11-17 09:26:00 +0100

    test(e2e): Use jest snapshotting for E2E tests (#124)

--- packages/e2e-tests/scenarios/basic-upload/ref/vite/~/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var i=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{};i.SENTRY_RELEASE={id:"basic-upload-vite"};const o=e=>{if(e===3)throw new Error("I'm an uncaught error");return e<=1?e:o(e-1)+o(e-2)};console.log("Hi, I'm a very simple app");o(10);console.log("I'm done");
-//# sourceMappingURL=index.js.map
