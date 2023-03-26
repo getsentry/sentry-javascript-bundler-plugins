69d85f6c9fd7217d5d59f49d0d935977cc3407a8
Luca Forstner <luca.forstner@sentry.io>
2023-03-26 07:53:56 +0400

    feat(core): Import release injection code from each module

---
+++ packages/bundler-plugin-core/src/release-injection-file.ts
@@ -0,0 +1,3 @@
+// This const is used for nothing except to make this file identifiable via its content.
+// We search for "_sentry_release_injection_file" in the plugin.
+export const _sentry_release_injection_file = true;
