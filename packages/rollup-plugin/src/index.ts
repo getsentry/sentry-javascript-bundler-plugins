12d80229caf9839c185241e40dae7d96132a4f5e
Luca Forstner <luca.forstner@sentry.io>
2023-05-02 15:34:02 +0200

    ref(core): Use factory function to create individual plugins (#229)

--- packages/rollup-plugin/src/index.ts
+++ packages/rollup-plugin/src/index.ts
@@ -1,2 +1,9 @@
-export { sentryRollupPlugin, sentryCliBinaryExists } from "@sentry/bundler-plugin-core";
+import { sentryUnpluginFactory, Options } from "@sentry/bundler-plugin-core";
+
+const sentryUnplugin = sentryUnpluginFactory();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const sentryRollupPlugin: (options: Options) => any = sentryUnplugin.rollup;
+
 export type { Options as SentryRollupPluginOptions } from "@sentry/bundler-plugin-core";
+export { sentryCliBinaryExists } from "@sentry/bundler-plugin-core";
