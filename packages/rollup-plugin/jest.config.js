f701dae79cf9f4aa63f1d45ef5c2c7aa0eadaa89
Luca Forstner <luca.forstner@sentry.io>
2023-06-06 13:38:54 +0200

    fix tests

--- packages/rollup-plugin/jest.config.js
+++ packages/rollup-plugin/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   testEnvironment: "node",
+  transformIgnorePatterns: [
+    "/node_modules/(?!(uuid)/)", // transpile uuid module to fix test failure
+    "\\.pnp\\.[^\\/]+$",
+  ],
   transform: {
     "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
