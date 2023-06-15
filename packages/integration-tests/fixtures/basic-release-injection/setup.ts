743bb1b0207db0be1828982a15e7a350e2410210
Luca Forstner <luca.forstner@sentry.io>
2023-06-15 15:53:10 +0200

    fix(esbuild): Don't override user code with proxy module (#322)

--- packages/integration-tests/fixtures/basic-release-injection/setup.ts
+++ packages/integration-tests/fixtures/basic-release-injection/setup.ts
@@ -5,5 +5,6 @@ const entryPointPath = path.resolve(__dirname, "input", "entrypoint.js");
 const outputDir = path.resolve(__dirname, "out");
 
 createCjsBundles({ index: entryPointPath }, outputDir, {
+  telemetry: false,
   release: { name: "I AM A RELEASE!", create: false },
 });
