c3d131df92004d901857658db6c2bb38bd049437
Luca Forstner <luca.forstner@sentry.io>
2023-06-16 11:14:11 +0200

    Add comment explaining variant

--- packages/bundler-plugin-core/src/utils.ts
+++ packages/bundler-plugin-core/src/utils.ts
@@ -175,7 +175,11 @@ export function stringToUUID(str: string): string {
   const md5sum = crypto.createHash("md5");
   md5sum.update(str);
   const md5Hash = md5sum.digest("hex");
-  const variant = ["8", "9", "a", "b"][md5Hash.substring(16, 17).charCodeAt(0) % 4] as string;
+
+  // Position 16 is fixed to either 8, 9, a, or b in the uuid v4 spec (10xx in binary)
+  // RFC 4122 section 4.4
+  const v4variant = ["8", "9", "a", "b"][md5Hash.substring(16, 17).charCodeAt(0) % 4] as string;
+
   return (
     md5Hash.substring(0, 8) +
     "-" +
@@ -183,7 +187,7 @@ export function stringToUUID(str: string): string {
     "-4" +
     md5Hash.substring(13, 16) +
     "-" +
-    variant +
+    v4variant +
     md5Hash.substring(17, 20) +
     "-" +
     md5Hash.substring(20)
