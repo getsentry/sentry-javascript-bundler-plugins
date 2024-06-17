8b49159165b0bd2926fcef915415372b50d9283a
Luca Forstner <luca.forstner@sentry.io>
2024-06-17 17:26:06 +0200

    maybee

--- packages/integration-tests/fixtures/after-upload-deletion/setup.ts
+++ packages/integration-tests/fixtures/after-upload-deletion/setup.ts
@@ -11,7 +11,7 @@ const outputDir = path.resolve(__dirname, "out");
     outputDir,
     {
       sourcemaps: {
-        filesToDeleteAfterUpload: path.join(__dirname, "out", bundler, "*.map"),
+        filesToDeleteAfterUpload: path.join(".", "**", "after-upload-deletion", "**", "*.map"),
       },
     },
     [bundler]
