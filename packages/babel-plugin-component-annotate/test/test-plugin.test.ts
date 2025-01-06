0e8c3296beb1c0baa93c8bca49e4590c9dba87e2
Ash Anand <ash.anand@sentry.io>
2025-01-06 17:11:31 -0500

    remove remaining redundant tests

--- packages/babel-plugin-component-annotate/test/test-plugin.test.ts
+++ packages/babel-plugin-component-annotate/test/test-plugin.test.ts
@@ -1051,260 +1051,6 @@ export default PureComponentName;
   expect(result?.code).toMatchSnapshot();
 });
 
-it("Bananas ignore components dataSentrySourceFile=nomatch dataSentryComponent=nomatch dataSentryElement=nomatch snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "nomatch", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("ignore components dataSentrySourceFile=* dataSentryComponent=nomatch dataSentryElement=nomatch snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["*", "nomatch", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("Bananas ignore components dataSentrySourceFile=nomatch dataSentryComponent=* dataSentryElement=nomatch snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "*", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("Bananas ignore components dataSentrySourceFile=nomatch dataSentryComponent=nomatch dataSentryElement=* snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "nomatch", "*"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("Bananas ignore components dataSentrySourceFile=* dataSentryComponent=* dataSentryElement=nomatch snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "nomatch", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("Bananas ignore components dataSentrySourceFile=* dataSentryComponent=nomatch dataSentryElement=* snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "nomatch", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-it("Bananas ignore components dataSentrySourceFile=nomatch dataSentryComponent=* dataSentryElement=* snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: [["nomatch.js", "nomatch", "nomatch"]] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\",
-          dataSentryElement: \\"Image\\",
-          dataSentryComponent: \\"Bananas\\",
-          dataSentrySourceFile: \\"filename-test.js\\"
-        });
-      }
-    }"
-  `);
-});
-
-// This tests out matching only `dataSentryElement`, with * for the others
-it("Bananas ignore components dataSentrySourceFile=* dataSentryComponent=* dataSentryElement=match snapshot matches", () => {
-  const result = transform(BananasStandardInput, {
-    filename: "/filename-test.js",
-    configFile: false,
-    presets: ["@babel/preset-react"],
-    plugins: [[plugin, { native: true, ignoreComponents: ["Image"] }]],
-  });
-  expect(result?.code).toMatchInlineSnapshot(`
-    "import React, { Component } from 'react';
-    import { Image } from 'react-native';
-    class Bananas extends Component {
-      render() {
-        let pic = {
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
-        return /*#__PURE__*/React.createElement(Image, {
-          source: pic,
-          style: {
-            width: 193,
-            height: 110,
-            marginTop: 10
-          },
-          fsClass: \\"test-class\\"
-        });
-      }
-    }"
-  `);
-});
-
 it("Bananas incompatible plugin @react-navigation source snapshot matches", () => {
   const result = transform(BananasStandardInput, {
     filename: "test/node_modules/@react-navigation/core/filename-test.js",
@@ -1330,5 +1076,100 @@ it("Bananas incompatible plugin @react-navigation source snapshot matches", () =
         });
       }
     }"
+  `);
+});
+
+it("skips components marked in ignoreComponents", () => {
+  const result = transform(BananasPizzaAppStandardInput, {
+    filename: "/filename-test.js",
+    presets: ["@babel/preset-react"],
+    plugins: [[plugin, { native: true, ignoreComponents: ["Bananas"] }]],
+  });
+  expect(result?.code).toMatchInlineSnapshot(`
+    "import React, { Component } from 'react';
+    import { StyleSheet, Text, TextInput, View, Image, UIManager } from 'react-native';
+    UIManager.getViewManagerConfig('RCTView').NativeProps.fsClass = \\"String\\";
+    class Bananas extends Component {
+      render() {
+        let pic = {
+          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return /*#__PURE__*/React.createElement(Image, {
+          source: pic,
+          style: {
+            width: 193,
+            height: 110,
+            marginTop: 10
+          },
+          fsClass: \\"test-class\\"
+        });
+      }
+    }
+    class PizzaTranslator extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          text: ''
+        };
+      }
+      render() {
+        return /*#__PURE__*/React.createElement(View, {
+          style: {
+            padding: 10
+          },
+          dataSentryElement: \\"View\\",
+          dataSentryComponent: \\"PizzaTranslator\\",
+          dataSentrySourceFile: \\"filename-test.js\\"
+        }, /*#__PURE__*/React.createElement(TextInput, {
+          style: {
+            backgroundColor: '#000',
+            color: '#eee',
+            padding: 8
+          },
+          placeholder: \\"Type here to translate!\\" // not supported on iOS
+          ,
+          onChangeText: text => this.setState({
+            text
+          }),
+          value: this.state.text,
+          dataSentryElement: \\"TextInput\\",
+          dataSentrySourceFile: \\"filename-test.js\\"
+        }), /*#__PURE__*/React.createElement(Text, {
+          style: {
+            padding: 10,
+            fontSize: 42
+          },
+          dataSentryElement: \\"Text\\",
+          dataSentrySourceFile: \\"filename-test.js\\"
+        }, this.state.text.split(' ').map(word => word && '🍕').join(' ')));
+      }
+    }
+    export default function App() {
+      return /*#__PURE__*/React.createElement(View, {
+        style: styles.container,
+        dataSentryElement: \\"View\\",
+        dataSentryComponent: \\"App\\",
+        dataSentrySourceFile: \\"filename-test.js\\"
+      }, /*#__PURE__*/React.createElement(Text, {
+        style: {
+          color: '#eee'
+        },
+        dataSentryElement: \\"Text\\",
+        dataSentrySourceFile: \\"filename-test.js\\"
+      }, \\"FullStory ReactNative testing app\\"), /*#__PURE__*/React.createElement(Bananas, null), /*#__PURE__*/React.createElement(PizzaTranslator, {
+        dataSentryElement: \\"PizzaTranslator\\",
+        dataSentrySourceFile: \\"filename-test.js\\"
+      }));
+    }
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#222',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
+    });"
   `);
 });
