21713038bdae814e269877a4649f94cc72041b02
Lukas Stracke <lukas.stracke@sentry.io>
2025-02-26 17:12:46 +0100

    run webpack 4 only on Node < 18

--- packages/integration-tests/fixtures/errorhandling/error-no-handler.test.ts
+++ packages/integration-tests/fixtures/errorhandling/error-no-handler.test.ts
@@ -2,6 +2,7 @@
 /* eslint-disable jest/expect-expect */
 import path from "path";
 import { spawn } from "child_process";
+import { testIfNodeMajorVersionIsLessThan18 } from "../../utils/testIf";
 
 jest.setTimeout(10_000);
 
@@ -26,10 +27,34 @@ describe("Error throwing by default (no errorHandler)", () => {
     sentryServer.kill();
   });
 
-  test.each(["vite", "rollup", "webpack5", "webpack4", "esbuild"])(
+  test.each(["vite", "rollup", "webpack5", "esbuild"])(
     "doesn't throw when Sentry server responds with error code for %s",
     async (bundler) => {
       const buildProcess = spawn("yarn", ["ts-node", path.join(__dirname, `build-${bundler}.ts`)], {
+        env: { ...process.env, FAKE_SENTRY_PORT },
+        stdio: "ignore", // <-- set to "inherit" to get build output. Deactivated to avoid spamming test logs.
+      });
+
+      const exitCode = await new Promise<number>((resolve, reject) => {
+        buildProcess.on("exit", (code) => {
+          resolve(code ?? 99);
+        });
+
+        buildProcess.on("error", (err) => {
+          reject(err);
+        });
+      });
+
+      expect(exitCode).toBe(0);
+
+      buildProcess.kill();
+    }
+  );
+
+  testIfNodeMajorVersionIsLessThan18(
+    "doesn't throw when Sentry server responds with error code for webpack 4",
+    async () => {
+      const buildProcess = spawn("yarn", ["ts-node", path.join(__dirname, `build-webpack4.ts`)], {
         env: { ...process.env, FAKE_SENTRY_PORT },
         stdio: "ignore", // <-- set to "inherit" to get build output. Deactivated to avoid spamming test logs.
       });
