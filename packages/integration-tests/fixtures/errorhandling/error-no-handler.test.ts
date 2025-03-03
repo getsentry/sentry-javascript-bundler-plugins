600f2abe0f5c869494a701afedbd0fc1dac8351e
Lukas Stracke <lukas.stracke@sentry.io>
2025-03-03 18:05:28 +0100

    run webpack 4 test

--- packages/integration-tests/fixtures/errorhandling/error-no-handler.test.ts
+++ packages/integration-tests/fixtures/errorhandling/error-no-handler.test.ts
@@ -2,7 +2,6 @@
 /* eslint-disable jest/expect-expect */
 import path from "path";
 import { spawn } from "child_process";
-import { testIfNodeMajorVersionIsLessThan18 } from "../../utils/testIf";
 
 jest.setTimeout(10_000);
 
@@ -28,35 +27,16 @@ describe("Error throwing by default (no errorHandler)", () => {
     sentryServer.kill();
   });
 
-  test.each(["vite", "rollup", "webpack5", "esbuild"])(
+  const bundlersToTest = ["vite", "rollup", "webpack5", "esbuild"];
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  if (parseInt(process.version.split(".")[0]!.slice(1)) < 18) {
+    bundlersToTest.push("webpack4");
+  }
+
+  test.each(bundlersToTest)(
     "doesn't throw when Sentry server responds with error code for %s",
     async (bundler) => {
       const buildProcess = spawn("yarn", ["ts-node", path.join(__dirname, `build-${bundler}.ts`)], {
-        env: { ...process.env, FAKE_SENTRY_PORT },
-        stdio: "ignore", // <-- set to "inherit" to get build output. Deactivated to avoid spamming test logs.
-        shell: true,
-      });
-
-      const exitCode = await new Promise<number>((resolve, reject) => {
-        buildProcess.on("exit", (code) => {
-          resolve(code ?? 99);
-        });
-
-        buildProcess.on("error", (err) => {
-          reject(err);
-        });
-      });
-
-      expect(exitCode).toBe(0);
-
-      buildProcess.kill();
-    }
-  );
-
-  testIfNodeMajorVersionIsLessThan18(
-    "doesn't throw when Sentry server responds with error code for webpack 4",
-    async () => {
-      const buildProcess = spawn("yarn", ["ts-node", path.join(__dirname, `build-webpack4.ts`)], {
         env: { ...process.env, FAKE_SENTRY_PORT },
         stdio: "ignore", // <-- set to "inherit" to get build output. Deactivated to avoid spamming test logs.
         shell: true,
