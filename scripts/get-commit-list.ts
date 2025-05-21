addf27c10b7a056ce087e513ba8524fd9dbc2bd3
Lukas Stracke <lukas.stracke@sentry.io>
2025-05-21 10:14:45 +0200

    actually add the sript lol

---
+++ scripts/get-commit-list.ts
@@ -0,0 +1,36 @@
+import { execSync } from "child_process";
+
+function run(): void {
+  const commits = execSync('git log --format="- %s"').toString().split("\n");
+
+  const lastReleasePos = commits.findIndex((commit) => /- meta(.*)changelog/i.test(commit));
+
+  const newCommits = commits.splice(0, lastReleasePos).filter((commit) => {
+    // Filter out merge commits
+    if (/Merge pull request/.test(commit)) {
+      return false;
+    }
+    // Filter release branch merged
+    if (/Merge branch/.test(commit)) {
+      return false;
+    }
+    // Filter release commit itself
+    if (/release:/.test(commit)) {
+      return false;
+    }
+
+    return true;
+  });
+
+  newCommits.sort((a, b) => a.localeCompare(b));
+
+  const issueUrl = "https://github.com/getsentry/sentry-javascript-bundler-plugins/pull/";
+  const newCommitsWithLink = newCommits.map((commit) =>
+    commit.replace(/#(\d+)/, `[#$1](${issueUrl}$1)`)
+  );
+
+  // eslint-disable-next-line no-console
+  console.log(newCommitsWithLink.join("\n"));
+}
+
+run();
