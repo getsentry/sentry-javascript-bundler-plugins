bcd33410c8e4de1ed231bcdd74f1cf907a3dc197
Lukas Stracke <lukas.stracke@sentry.io>
2025-10-09 13:09:08 +0200

    check and add

--- packages/integration-tests/fixtures/debug-ids-already-injected/input/rollup4/rollup.config.js
+++ packages/integration-tests/fixtures/debug-ids-already-injected/input/rollup4/rollup.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from "rollup";
 import { sentryRollupPlugin } from "@sentry/rollup-plugin";
-import { join, posix } from "path";
+import { join } from "path";
+import { existsSync } from "fs";
 
 const __dirname = new URL(".", import.meta.url).pathname;
 
 console.log({
   __dirname,
-  posixInput: posix.join(__dirname, "bundle.js"),
-  input: join(__dirname, "..", "bundle.js"),
+  input: join(__dirname, "bundle.js"),
+  existsInput: existsSync(join(__dirname, "bundle.js")),
   outputPath: join(__dirname, "..", "..", "out", "rollup4"),
-  posixOutputPath: posix.join(__dirname, "..", "..", "out", "rollup4"),
 });
 
 export default defineConfig({
-  input: { index: posix.join(__dirname, "bundle.js") },
+  input: { index: join(__dirname, "bundle.js") },
   output: {
-    dir: posix.join(__dirname, "..", "..", "out", "rollup4"),
+    dir: join(__dirname, "..", "..", "out", "rollup4"),
     sourcemap: true,
     sourcemapDebugIds: true,
   },
