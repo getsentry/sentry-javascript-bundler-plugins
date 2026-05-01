fd7eeb8087595c9e1164a76a3c8ad635d7675247
Tim Fish <tim@timfish.uk>
2026-05-02 00:04:14 +0200

    missing snapshots

--- packages/integration-tests-next/fixtures/rollup4/basic-sourcemaps.test.ts
+++ packages/integration-tests-next/fixtures/rollup4/basic-sourcemaps.test.ts
@@ -10,7 +10,7 @@ test(import.meta.url, ({ runBundler, readOutputFiles, runFileInNode }) => {
     //# sourceMappingURL=basic.js.map
     ",
       "basic.js.map": "{"version":3,"file":"basic.js","sources":["../../src/basic.js"],"sourcesContent":["// eslint-disable-next-line no-console\\nconsole.log(\\"hello world\\");\\n"],"names":[],"mappings":"AAAA,CAAA,CAAA,CAAA,MAAA,CAAA,OAAA,CAAA,IAAA,CAAA,IAAA,CAAA,EAAA,CAAA;scACA,OAAO,CAAC,GAAG,CAAC,CAAA,KAAA,CAAA,KAAA,CAAa,CAAC"}",
-      "sentry-cli-mock.json": "["releases","new","CURRENT_SHA"],
+      "sentry-cli-mock.json": "["releases","new","CURRENT_SHA","-p","fake-project"],
     ["releases","set-commits","CURRENT_SHA","--auto","--ignore-missing"],
     ["releases","finalize","CURRENT_SHA"],
     ["sourcemaps","upload","-p","fake-project","--release","CURRENT_SHA","sentry-bundler-plugin-upload-path","--ignore","node_modules","--no-rewrite"],
