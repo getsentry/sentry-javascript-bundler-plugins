054ac7be2056dfb6bfa49b306c4fc4405b0eefdf
Tim Fish <tim@timfish.uk>
2026-05-04 14:29:47 +0200

    Revert bad find+replace!

--- packages/integration-tests-next/fixtures/esbuild/utils.ts
+++ packages/integration-tests-next/fixtures/esbuild/utils.ts
@@ -37,7 +37,7 @@ export function test(url: string, callback: TestCallback) {
   // Detect CJS config files by test name suffix
   const configExt = testName.endsWith("-cjs") ? ".config.cjs" : ".config.js";
 
-  vitestTest(`pnpm esbuild > ${testName}`, (ctx) =>
+  vitestTest(`esbuild > ${testName}`, (ctx) =>
     callback({
       outDir,
       runBundler: (env) =>
