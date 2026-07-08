b0be3c79b7097cb31a2afc1dd719dcd9b6018939
Tim Fish <tim@timfish.uk>
2026-07-08 12:01:40 +0200

    feat: Depend on JavaScript repo package. (#945)

--- packages/bundler-plugins/src/core/glob.ts
+++
@@ -1,8 +0,0 @@
-import { glob } from "glob";
-
-export function globFiles(
-  patterns: string | string[],
-  options?: { root?: string; ignore?: string | string[] }
-): Promise<string[]> {
-  return glob(patterns, { absolute: true, nodir: true, ...options });
-}
